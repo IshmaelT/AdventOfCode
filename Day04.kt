579789c39c0404ff56c58ba648bc44de0a229b33
Ishmael Tshikhovhokhovho <ishmaelt@luno.com>
2022-12-04 14:56:11 +0200

    day04 part 2

--- Day04.kt
+++ Day04.kt
@@ -1,4 +1,3 @@
-import java.io.BufferedReader
 import java.io.FileReader
 
 fun main() {
@@ -7,14 +6,36 @@ fun main() {
         .readText()
         .lines()
 
+    operator fun String.component1() = split("-")[0]
+    operator fun String.component2() = split("-")[1]
+
     var count = 0
     assignmentPairs.forEach {
-        if(it.isBlank()) return@forEach
-        val (a,b) = it.split(',')
+        if (it.isBlank()) return@forEach
+        val (a, b) = it.split(',')
+        val (aInit, aFinal) = a
+        val (bInit, bFinal) = b
+        val firstElf = aInit.toInt()..aFinal.toInt()
+        val secondElf = bInit.toInt()..bFinal.toInt()
+        val intersection = firstElf intersect secondElf
+        if (intersection.isNotEmpty()) {
+            println(it)
+            println("${intersection.first()}-${intersection.last()}")
+            count++
+        }
+    }
+    println("Pairs with overlap = $count")
+}
+
+private fun partOne(assignmentPairs: List<String>) {
+    var count = 0
+    assignmentPairs.forEach {
+        if (it.isBlank()) return@forEach
+        val (a, b) = it.split(',')
         val (aInit, aFinal) = a.split("-")
         val (bInit, bFinal) = b.split("-")
-        val firstElf = (aInit.toInt() .. aFinal.toInt())
-        val secondElf = (bInit.toInt() ..bFinal.toInt())
+        val firstElf = (aInit.toInt()..aFinal.toInt())
+        val secondElf = (bInit.toInt()..bFinal.toInt())
         val intersection = firstElf intersect secondElf
         if (intersection.size == firstElf.toList().size || intersection.size == secondElf.toList().size) {
             println(it)
