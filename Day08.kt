a7a1e1c4a49dea4776b482698f9c89b5f7d0d125
Ishmael Tshikhovhokhovho <ishmaelt@luno.com>
2022-12-09 23:23:41 +0200

    day 8 part 2

--- Day08.kt
+++ Day08.kt
@@ -42,3 +42,38 @@ fun main() {
     println(unique)
     println(unique.size + (2 * rows.size) + (2 * (rows.first().size - 2)) )
 }
+
+private fun part2(rows: MutableList<List<Int>>) {
+    val unique = mutableSetOf<Pair<String, Int>>()
+    for ((rowNumber, row) in rows.withIndex()) {
+        for ((columnNumber, number) in row.withIndex()) {
+            if (rowNumber in 1 until (rows.size - 1) &&
+                columnNumber in 1 until (row.size - 1)
+            ) {
+
+                val (left, _) = row.withIndex().partition { it.index < columnNumber }
+                val (right, _) = row.withIndex().partition { it.index > columnNumber }
+
+                val column = rows.map { it[columnNumber] }
+
+                val (top, _) = column.withIndex().partition { it.index < rowNumber }
+                val (bottom, _) = column.withIndex().partition { it.index > rowNumber }
+
+                val countLeft =
+                    left.asReversed().indexOfFirst { it.value >= number }.let { if (it == -1) left.size else (it + 1) }
+                val countRight =
+                    right.indexOfFirst { it.value >= number }.let { if (it == -1) right.size else (it + 1) }
+                val countTop =
+                    top.asReversed().indexOfFirst { it.value >= number }.let { if (it == -1) top.size else (it + 1) }
+                val countBottom =
+                    bottom.indexOfFirst { it.value >= number }.let { if (it == -1) bottom.size else (it + 1) }
+
+                val answer = countLeft * countRight * countTop * countBottom
+
+                unique.add("($rowNumber,$columnNumber)" to answer)
+            }
+        }
+    }
+
+    println(unique.maxOf { it.second })
+}
